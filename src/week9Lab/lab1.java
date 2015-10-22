4408ba0c1dc01de0a61255f85260ef70eb089f44
a213 <a213@ISA2130029.sac.rsccd.inst>
2015-10-21 19:25:18 -0700

    725 1 commit

---
+++ src/week9Lab/lab1.java
@@ -0,0 +1,32 @@
+package week9Lab;
+
+import java.util.Arrays;
+
+public class lab1 {
+	
+	public static void main(String[] args){
+		
+		
+		int[] myArray= {123,456,479,135,246,135};
+		
+		int sum=0;
+		Arrays.sort(myArray);
+		
+		for (int i=0;i<myArray.length;i++){
+			
+			
+			System.out.printf("This is the number %d\n",myArray[i]);
+			sum = sum +myArray[i];
+			
+		}
+		
+		float average = sum/myArray.length;
+	
+		System.out.printf("The average is %.2f", average);
+		
+		
+		
+		
+	}
+
+}
